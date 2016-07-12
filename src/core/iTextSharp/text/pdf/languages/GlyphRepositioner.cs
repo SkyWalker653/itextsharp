93699483f2e4ac8602169a18c0aa3430bce0284e
Priya Ranjan <priyaranjan.653@gmail.com>
2016-07-12 16:16:00 +0530

    Add files via upload

---
+++ src/core/iTextSharp/text/pdf/languages/GlyphRepositioner.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace iTextSharp.text.pdf.languages
+{
+    public interface GlyphRepositioner
+    {
+        void repositionGlyphs(List<Glyph> glyphList);
+    }
+}
