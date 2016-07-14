f70ac9414cb520522f059220d59cbfc2cb390a8c
Priya Ranjan <priyaranjan.653@gmail.com>
2016-07-14 14:59:06 +0530

    Add files via upload

---
+++ src/core/iTextSharp/text/pdf/languages/IndicCompositeCharacterComparator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace iTextSharp.text.pdf.languages
+{
+    public class IndicCompositeCharacterComparator : IComparer<String>
+    {
+        public int Compare(String o1, String o2)
+        {
+            if (o2.Length > o1.Length)
+            {
+                return 1;
+            }
+            else if (o1.Length > o2.Length)
+            {
+                return -1;
+            }
+            else
+            {
+                return Compare(o1, o2);
+            }
+        }
+    }
+}
